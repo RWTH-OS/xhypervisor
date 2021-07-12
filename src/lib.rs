5c2bf805a5b576bda61c0f586fc3723f89749740
Martin Kröning <mkroening@posteo.net>
2021-07-12 13:52:48 +0200

    Derive std::error::Error for Error

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,7 @@ in your Terminal:
 
 extern crate core;
 extern crate libc;
+extern crate thiserror;
 
 pub mod consts;
 #[allow(non_camel_case_types)]
@@ -51,40 +52,27 @@ pub mod ffi;
 
 use self::core::fmt;
 use libc::*;
+use thiserror::Error;
 
 use self::ffi::*;
 
 /// Error returned after every call
-#[derive(Clone)]
+#[derive(Error, Debug)]
 pub enum Error {
-	/// Success
+	#[error("success")]
 	Success,
-	/// Error
+	#[error("error")]
 	Error,
-	/// Busy
+	#[error("busy")]
 	Busy,
-	/// Bad argument
+	#[error("bad argument")]
 	BadArg,
-	/// No resources
+	#[error("no resource")]
 	NoRes,
-	/// No device
+	#[error("no device")]
 	NoDev,
-	/// Unsupported
+	#[error("unsupported")]
 	Unsupp,
-}
-
-impl fmt::Debug for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match *self {
-			Error::Success => write!(f, "Success"),
-			Error::Error => write!(f, "Error"),
-			Error::Busy => write!(f, "Busy"),
-			Error::BadArg => write!(f, "Bad argument"),
-			Error::NoRes => write!(f, "No resources"),
-			Error::NoDev => write!(f, "No device"),
-			Error::Unsupp => write!(f, "Unsupported"),
-		}
-	}
 }
 
 // Returns an Error for a hv_return_t
